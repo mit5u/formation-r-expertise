d7aed8fb593b931eaf3f9c7cd8669b523ab98920
mit5u <zaku.ichiyo@gmail.com>
2024-02-13 17:40:41 +0100

    Closes #1

--- R/seq1_ex_4_2.R
+++ R/seq1_ex_4_2.R
@@ -1,15 +1,34 @@
 load(file = file.path(PATH_FINAL, "revenus_boxplot.RData"))
 
 calculer_stat <- function(data, ...) {
-  dots <- rlang::list2(...)
-  
-  revenus_dep_2014 %>% 
-    reframe(across(where(is.numeric), dots)) %>% 
-    # comment suffixer les variables avec la fonction-résumé utilisée ?
-    pivot_longer(cols = everything(),
-                 names_to = "variable",
-                 values_to = "valeur")
+    dots <- rlang::dots_list(...,
+                             .named = TRUE, # pour avoir en colonne le noms des fonctions correspondantes, plutôt qu'un numéro
+                             .homonyms = "first")
+    
+    data %>%
+        reframe(across(where(is.numeric),
+                       dots)
+        ) %>% 
+        pivot_longer(cols = everything(),
+                     names_to = c("variable", "stat"),
+                     names_pattern = "(.*)_(.*)"
+        ) %>% 
+        pivot_wider(id_cols = everything(),
+                    names_from = "stat",
+                    values_from = "value")
 }
 
 calculer_stat(revenus_dep_2014, min, max, median)
-calculer_stat(revenus_dep_2014, sd, var, min)
+
+# les fonctions custom peuvent être définies en amont
+cv <- function(x, na.rm = TRUE) {
+    sd(x, na.rm = na.rm) / mean(x, na.rm = na.rm)
+}
+revenus_dep_2014 %>% 
+    calculer_stat(sd, sum, cv)
+# on peut aussi utiliser des fonctions anonymes à la volée, mais le résultat sera laid :
+revenus_dep_2014 %>% 
+    calculer_stat(sd,
+                  sum,
+                  (\(x) return(sd(x, na.rm = TRUE) / mean(x, na.rm = TRUE)))
+    )
